5dd1d4faa84ef8ab31d8f6bcd04615ed3f6bba6c
David Lewis <dlewis@civisanalytics.com>
2018-01-22 22:27:52 +0000

    level based logging

--- app.py
+++ app.py
@@ -2,7 +2,30 @@ import logging
 import sys
 from flask import Flask, render_template, request, jsonify
 
-logging.basicConfig(stream=sys.stdout)
+
+class LevelFilter(logging.Filter):
+    def __init__(self, *args):
+        self.levels = args
+
+    def filter(self, record):
+        return record.levelno in self.levels
+
+
+h1 = logging.StreamHandler(sys.stdout)
+f1 = LevelFilter(logging.DEBUG, logging.INFO, logging.WARNING)
+h1.addFilter(f1)
+
+h2 = logging.StreamHandler(sys.stderr)
+f2 = LevelFilter(logging.ERROR, logging.CRITICAL)
+h2.addFilter(f2)
+
+rootLogger = logging.getLogger()
+rootLogger.addHandler(h1)
+rootLogger.addHandler(h2)
+
+logger = logging.getLogger('test')
+logger.setLevel(logging.DEBUG)
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -13,12 +36,11 @@ def index():
 def log():
     data = request.get_json()
     message = data['message']
-    times = data['times']
-    for i in range(0, int(times)):
-        log_message = message + ' ' + str(i)
-        logging.error(log_message)
+    level = data['level']
 
-    return jsonify({'status': 200})
+    getattr(logger, level)(message)
+
+    return jsonify({'message': 'Logged message successfully'})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3838)
