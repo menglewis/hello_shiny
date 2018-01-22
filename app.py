f838681deedca2c231b9122b019463f99a4c7593
David Lewis <dlewis@civisanalytics.com>
2018-01-22 18:23:22 +0000

    use ajax to get past iframe form restrictions

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import logging
 import sys
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 logging.basicConfig(stream=sys.stdout)
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        message = request.form['message']
-        times = request.form['times']
-
-        for i in range(0, int(times)):
-            log_message = message + ' ' + str(i)
-            logging.error(log_message)
-
     return render_template('index.html')
+
+@app.route('/log', methods=['POST'])
+def log():
+    data = request.get_json()
+    message = data['message']
+    times = data['times']
+    for i in range(0, int(times)):
+        log_message = message + ' ' + str(i)
+        logging.error(log_message)
+
+    return jsonify({'status': 200})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3838)
